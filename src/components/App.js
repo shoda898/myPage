1227fd2a3d6b71c05add866c26599ae5526a0c86
shoda898 <75591054+shoda898@users.noreply.github.com>
2021-06-02 21:08:31 +0900

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
+// Reactをインポートしてください
 import React from 'react';
 
+// Appクラスを定義してください
 class App extends React.Component {
-  render() {
+  render(){
     return (
-      <div>
-        <h1>Hello React</h1>
-        <p>一緒にReacctを学びましょう</p>
-      </div>
+      <h1>Hello React</h1>
     );
   }
 }
 
+// Appクラスをエクスポートしてください
 export default App;
